7e162097d76ba48f1077b9c23d1bad6e92867e86
JosipHo0rvat <josiph988@gmail.com>
2022-02-01 22:12:52 +0000

    First commit: created project, home page, form, Tour and display list of tours.

--- src/main/java/com/josip/travelagency/controllers/HomeController.java
+++ src/main/java/com/josip/travelagency/controllers/HomeController.java
@@ -3,8 +3,13 @@ package com.josip.travelagency.controllers;
 import com.josip.travelagency.model.Tour;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +21,7 @@ public class HomeController {
 
     //http://localhost:8080/
     @RequestMapping("/")
-    public String getHome(){
+    public String getHome() {
         return "home";
     }
 
@@ -27,7 +32,11 @@ public class HomeController {
     }
 
     @PostMapping("processForm")
-    public String showTourData(@ModelAttribute Tour tour) {
+    public String showTourData(@Valid @ModelAttribute Tour tour, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "form";
+        }
         tours.add(tour);
         return "redirect:showOffer";
     }
--- src/main/java/com/josip/travelagency/model/Tour.java
+++ src/main/java/com/josip/travelagency/model/Tour.java
@@ -1,61 +1,78 @@
 package com.josip.travelagency.model;
-
+import static com.josip.travelagency.constants.TourErrorMessage.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.*;
 import java.util.Date;
 
 public class Tour {
 
-    public enum Continent {
-        AFRICA, ASIA, EUROPE, NORTH_AMERICA, SOUTH_AMERICA, AUSTRALIA;
-    }
-
+    @NotBlank(message = TOUR_NAME_NOT_NULL)
+    @Size(min = 5, message = TOUR_STRING_SIZE)
     private String name;
+    @Pattern(regexp = "^[a-zA-Z]{2}-[0-9]{2}[a-zA-Z]{1}$", message = TOUR_CODE_PATTERN)
     private String code;
     private Continent continent;
+    @NotNull(message = TOUR_DATE_NOT_NULL)
+    @Future(message = TOUR_DATE_FUTURE)
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date date;
+    @Min(value = 7, message = TOUR_DURATION)
+    @Max(value = 21, message = TOUR_DURATION)
     private int duration;
     private boolean allInclusive = false;
-
 
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getCode() {
         return code;
     }
+
     public void setCode(String code) {
         this.code = code;
     }
+
     public Continent getContinent() {
         return continent;
     }
+
     public void setContinent(Continent continent) {
         this.continent = continent;
     }
+
     public Date getDate() {
         return date;
     }
+
     public void setDate(Date date) {
         this.date = date;
     }
+
     public int getDuration() {
         return duration;
     }
+
     public void setDuration(int duration) {
         this.duration = duration;
     }
+
     public boolean isAllInclusive() {
         return allInclusive;
     }
+
     public void setAllInclusive(boolean allInclusive) {
         this.allInclusive = allInclusive;
     }
 
+    public enum Continent {
+        AFRICA, ASIA, EUROPE, NORTH_AMERICA, SOUTH_AMERICA;
+    }
 
 
 }
