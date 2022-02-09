63796a9990c9bfba13d1d438285befdd4167433b
JosipHo0rvat <josiph988@gmail.com>
2022-02-09 19:49:57 +0000

    Security part 2

--- src/main/java/com/josip/travelagency/security/SecurityConfig.java
+++ src/main/java/com/josip/travelagency/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.josip.travelagency.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -21,6 +22,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
         auth
                 .inMemoryAuthentication()
-                .withUser("Josip").password(passwordEncoder().encode("admin")).roles("ADMIN");
+                .withUser("Josip").password(passwordEncoder().encode("admin")).roles("ADMIN, EMPLOYEE")
+                .and()
+                .withUser("Mile").password(passwordEncoder().encode("employee")).roles("EMPLOYEE")
+                .and()
+                .withUser("Pavo").password(passwordEncoder().encode("client")).roles("CLIENT");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       http.authorizeRequests()
+                   .antMatchers("/", "/login")
+                   .permitAll()
+               .and()
+                   .formLogin()
+                   .defaultSuccessUrl("/")
+                   .permitAll()
+               .and()
+                   .logout()
+                   .logoutSuccessUrl("/")
+                   .invalidateHttpSession(true)
+                   .permitAll();
     }
 }
